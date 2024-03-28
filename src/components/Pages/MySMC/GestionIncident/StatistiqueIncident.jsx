5419c06687ba3b99b495be49f746d1e2ad2842c1
Amy FALL [tmp_fall70327 DSI/SMC/SMM] <lzoae2fof4fvm1ua40nbpm5xe9f0qk@bots.bitbucket.org>
2024-03-28 11:48:10 +0000

    gestionErrorMessage signin

--- src/components/Pages/MySMC/GestionIncident/StatistiqueIncident.jsx
+++ src/components/Pages/MySMC/GestionIncident/StatistiqueIncident.jsx
@@ -8,7 +8,7 @@ import ReportProblemIcon from '@mui/icons-material/ReportProblem';
 import { RiDashboard3Line } from "react-icons/ri";
 import { IoStatsChart } from "react-icons/io5";
 import Title from '../../../Card/Title/Title'
-import { Container,Row,Col,Button } from 'react-bootstrap'
+import { Container,Row,Col,Button, Card } from 'react-bootstrap'
 import {InputLabel,TextField,Grid} from '@mui/material'
 
 
@@ -26,13 +26,26 @@ const ajoutAvisItemsMenu =[
       ];
 
 
+
+
 function StatistiqueIncident() {
-    useAuth()
+
     const [currentForm, setCurrentForm] = useState("")
+    const [text,setText]=useState("Information : Merci d'effectuer une recherche au préalable pour afficher les avis");
+
     const handleMenuClick = (link)=>{
         setCurrentForm(link);
         console.log(link);
       }
+
+      
+  const handlesearcclick=()=>{
+    const datedebut=document.getElementById("dateDebut").value
+    const datefin=document.getElementById("dateFin").value
+    setText(`Resultat de la dernière recherche :  Date Fin : ${datefin} | Date début : ${datedebut}` )
+
+    console.log(datedebut,datefin);
+  }
   return (
 <div id='home'>
      <Header/>
@@ -45,16 +58,23 @@ function StatistiqueIncident() {
           <div style={{display:"flex"}}>
              <div className='mb-4' >
              <InputLabel sx={{marginLeft:"6%"}} id="demo-simple-select-label">Date début</InputLabel>&nbsp;
-             <TextField variant='outlined'  size='small' type='date' sx={{width:"210px",marginRight:"30px"}}/>
+             <TextField id='dateDebut' variant='outlined'  size='small' type='date' sx={{width:"210px",marginRight:"30px"}}/>
              </div>
              <div className='mb-4' >
              <InputLabel sx={{marginLeft:"6%"}} id="demo-simple-select-label">Date Fin</InputLabel>&nbsp;
-             <TextField variant='outlined'  size='small' type='date' sx={{width:"210px",marginRight:"30px"}}/>
+             <TextField id='dateFin'variant='outlined'  size='small' type='date' sx={{width:"210px",marginRight:"30px"}}/>
              </div>
              <div  className='mb-4' id='search'>
-              <Button style={{ backgroundColor:"#d9534f", borderColor: " #d9534f",width:"210px"}}><FaSearch/>Obtenir Statistiques</Button> 
+              <Button style={{ backgroundColor:"#d9534f", borderColor: " #d9534f",width:"210px"}} onClick={handlesearcclick}><FaSearch/>Obtenir Statistiques</Button> 
              </div>
           </div>
+          <div className='col-xs-12 col-sm-6 col-md-4' style={{width:"100%"}}>
+              <Grid >
+                <h5 className=' alert alert-info' style={{fontSize:"14px",fontFamily:"inherit",fontWeight:"500",color:"#31708F"}}>
+                  {text}
+                 </h5>
+              </Grid>
+            </div>
           </Col>
           <Col sm={4}>
           <MenuPersoGesIncident  propsMenuItems={ajoutAvisItemsMenu} onItemClick={handleMenuClick}  />
@@ -62,58 +82,19 @@ function StatistiqueIncident() {
         </Row>
         <Row>
           <Col sm={8}>
-          <div className='col-xs-12 col-sm-6 col-md-4' style={{position:"absolute",width:"58%"}}>
-              <Grid >
-                <h5 className=' alert alert-info' style={{fontSize:"14px",fontFamily:"inherit",fontWeight:"500",color:"#31708F"}}>
-                 Information : Merci d'effectuer une recherche au préalable pour afficher les avis
-                 </h5>
-              </Grid>
-            </div>
+            <Row>
+            <Col ><Card style={{height:"100%" }}></Card></Col>
+            <Col ><Card style={{height:"100%" }}>Hello</Card></Col>
+            <Col ><Card style={{height:"100%" }}>Hello</Card></Col>
+          </Row>
            
           </Col>
           <Col sm={4}>
           <NavigatePerso  propsMenuItems={gestionIncidentItemsNavigate} onItemClick={handleMenuClick} />
           </Col>
         </Row>
+        
        </Container>
-        {/* <div style={{position:"relative"}}>
-        <MenuPersoGesIncident  propsMenuItems={ajoutAvisItemsMenu} onItemClick={handleMenuClick}  />
-        </div>
-        <div style={{position:"relative",top:"30%"}}>
-        <NavigatePerso  propsMenuItems={gestionIncidentItemsNavigate} onItemClick={handleMenuClick} />
-        </div>
-        <Container>
-        <div id='title'>
-            <Row>
-            <Col xs={9} className='content'>
-            <Title text="Historique des avis"/>
-            </Col> 
-            </Row>
-          </div>
-            <br />
-            <div style={{display:"flex"}}>
-            <div className='mb-4' >
-             <InputLabel sx={{marginLeft:"6%"}} id="demo-simple-select-label">Date début</InputLabel>&nbsp;
-             <TextField variant='outlined'  size='small' type='date' sx={{width:"250px",marginRight:"35px"}}/>
-             </div>
-             <div className='mb-4' >
-             <InputLabel sx={{marginLeft:"6%"}} id="demo-simple-select-label">Date Fin</InputLabel>&nbsp;
-             <TextField variant='outlined'  size='small' type='date' sx={{width:"250px",marginRight:"35px"}}/>
-             </div>
-             <div  className='mb-4' id='search'>
-              <Button style={{ backgroundColor:"#d9534f", borderColor: " #d9534f",width:"240px"}}><FaSearch/>Obtenir Statistiques</Button> 
-             </div>
-            
-        </div>
-            <div className='col-xs-12 col-sm-6 col-md-4' style={{position:"absolute",width:"66%"}}>
-              <Grid >
-                <h5 className=' alert alert-info' style={{fontSize:"14px",fontFamily:"inherit",fontWeight:"500",color:"#31708F"}}>
-                 Information : Merci d'effectuer une recherche au préalable pour afficher les avis
-                 </h5>
-              </Grid>
-            </div>
-           
-        </Container> */}
  </div>
   )
 }
