8c5efdc8de6b76755962aa847391d30186fd793a
Abdou Aziz SEYE [seye028962 DESC/DIS/ISDC] <abdouaziz.seye2@orange-sonatel.com>
2024-05-21 08:45:03 +0000

    delete comments

--- src/components/Pages/Kaabu/kaabu.service.js
+++ src/components/Pages/Kaabu/kaabu.service.js
@@ -6,14 +6,3 @@ const selfserviceUrl = 'abela-selfservice/api/v1';
 export default function getAllInfoUser(identifiant) {
     return httpClient.get(selfserviceUrl + '/kaabu/verification-user/'+identifiant).then(response => response ? response.data : null);
 }
-
-
-
-// const GetData = async (url) => {
-//     const response = await httpClient.get(url);
-//     if(response.data.success) {
-//       setUserkaabu(response.data.data.userKaabu);
-//       setSimplissimo(response.data.data.userSimplissimo)
-//     }
-//     return response.data; 
-//   };
