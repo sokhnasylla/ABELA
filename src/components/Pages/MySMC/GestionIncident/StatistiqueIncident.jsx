89b340a3688ebb9e1ed1c67eb2c813da17795345
SombreGhost <amadousallgueye123@gmail.com>
2024-09-04 09:23:40 +0000

    Menu DetailsIncident, update fonction for title display

--- src/components/Pages/MySMC/GestionIncident/StatistiqueIncident.jsx
+++ src/components/Pages/MySMC/GestionIncident/StatistiqueIncident.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import useAuth from "../../Auth/useAuth";
-import Header from "../../../Header/Header";
 import { FaList, FaSearch, FaHome, FaPaperclip } from "react-icons/fa";
 import ReportProblemIcon from "@mui/icons-material/ReportProblem";
 import { RiDashboard3Line } from "react-icons/ri";
@@ -8,7 +6,6 @@ import { IoStatsChart } from "react-icons/io5";
 import Title from "../../../Card/Title/Title";
 import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 import { InputLabel, TextField, Grid } from "@mui/material";
-import Taux from "../../../Card/Taux";
 import {
   BarChart,
   Bar,
@@ -108,16 +105,16 @@ function StatistiqueIncident() {
     "Information : Merci d'effectuer une recherche au préalable pour afficher les avis"
   );
   const [showStatModal, setShowStatModal] = useState(false);
-  const [histo, setHisto] = useState("Aucune recherche récente.");
   const now = new Date();
   const period = now.toLocaleDateString('FR', { month: 'long', year: 'numeric' });
   const start = new Date(now.getFullYear(), now.getMonth(), 1)
-    .toISOString()
-    .split("T")[0];
+  .toISOString()
+  .split("T")[0];
   const end = new Date(now.getFullYear(), now.getMonth() + 1, 0)
-    .toISOString()
-    .split("T")[0];
-
+  .toISOString()
+  .split("T")[0];
+  
+  const [histo, setHisto] = useState(period);
   const [dateDebut, setDateDebut] = useState(start);
   const [dateFin, setDateFin] = useState(end);
   const [dataUrl, setDataUrl] = useState(
@@ -153,6 +150,7 @@ function StatistiqueIncident() {
       `http://localhost:8082/abela-mysmc/api/v1/gestionIncidents/avisIncident/statistique/search?dateDebut=${dateDebut}&dateFin=${dateFin}`
     );
     setShowStatModal(false);
+    setHisto(period);
   };
   useEffect(() => {
     const fetchData = async () => {
@@ -194,71 +192,14 @@ function StatistiqueIncident() {
     { name: "Notification", value: totalAvisClosNotificationOnDelayCustom },
   ];
 
-  const getPeriode = (dateDebut, dateFin) => {
-    const dateDebutSplit = dateDebut.split("-");
-    const dateFinSplit = dateFin.split("-");
-    const mois = [
-      "Janvier",
-      "Février",
-      "Mars",
-      "Avril",
-      "Mai",
-      "Juin",
-      "Juillet",
-      "Août",
-      "Septembre",
-      "Octobre",
-      "Novembre",
-      "Décembre",
-    ];
-    console.log(histo);
-    const moisDebut = mois[parseInt(dateDebutSplit[1]) - 1];
-    const moisFin = mois[parseInt(dateFinSplit[1]) - 1];
-    return `Du ${dateDebutSplit[2]} ${moisDebut} ${dateDebutSplit[0]} au ${dateFinSplit[2]} ${moisFin} ${dateFinSplit[0]}`;
-  };
-
-  const getCurrentMonthAndYear = () => {
-    const date = new Date();
-    const mois = [
-      "Janvier",
-      "Février",
-      "Mars",
-      "Avril",
-      "Mai",
-      "Juin",
-      "Juillet",
-      "Août",
-      "Septembre",
-      "Octobre",
-      "Novembre",
-      "Décembre",
-    ];
-    const moisActuel = mois[date.getMonth()];
-    const anneeActuelle = date.getFullYear();
-    return `${moisActuel} ${anneeActuelle}`;
-  };
-
   return (
     <div className="dashboard">
-            {histo === "Aucune recherche récente." ? (
-          <Title
-            lg={12}
-            text={`Gestion des avis d'incidents - Indicateurs du mois en cours : ${period}`}
-          />
-        ) : histo !== "" ? (
-          // Si les dates de début et de fin sont présentes dans l'historique
-          <Title
+         <Title
             text={`Gestion des avis d'incidents - Indicateurs de la période : ${histo}`}
           />
-        ) : (
-          // Si aucune date n'a été choisie, afficher un titre générique
-          <Title
-            text={`Gestion des avis d'incidents - Indicateurs sans période définie`}
-          />
-        )}
       <Button  variant="primary" onClick={handleStatShow} className="mt-5 ml-5 mb-2">
               Stats      
-              </Button>
+      </Button>
        <Modal
               show={showStatModal}
               onHide={handleStatClose}
@@ -420,7 +361,6 @@ function StatistiqueIncident() {
           <Bar dataKey="value" fill="#FFA500" />
         </BarChart>
       </ResponsiveContainer>
-      {histo}
     </div>
   );
 }
